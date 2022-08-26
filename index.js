e6da8b92b67410c1ac44200aa4bcc70bdb5700d5
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-26 11:46:07 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+for (let i = 0; i < array.length; i++) {
+  const element = target - array[i];
+  for (let j = i+1; j < array.length; j++) {
+    if( array[j]=== element){
+      return true
+    }   
+  }
 }
+return false
+
+}
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +19,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+
+  1. loop through the array, 
+  2  for each element in an array, find a number that when added returns the target number
+  3. itterate through the array and find the number.
+  4. if the number exists in our array, return true, else return false
 */
 
 /*
   Add written explanation of your solution here
+  time complexity = O(n**2) since it has nested loops
+  space complexity = linear complexity because in worst space scenario if the length of array is very big, youll require a large memory to store the data O(n)
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +48,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
